e3785b1b8565c49fff3ad662679e031aefbd207c
wanglei288 <wanglei59@jd.com>
2017-07-05 17:48:50 +0800

    update spring boot & cloud version

--- service-admin/src/main/java/com/lovnx/NotifierConfiguration.java
+++ service-admin/src/main/java/com/lovnx/NotifierConfiguration.java
@@ -1,34 +1,32 @@
 package com.lovnx;
 
+import de.codecentric.boot.admin.notify.Notifier;
+import de.codecentric.boot.admin.notify.RemindingNotifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.netflix.governator.annotations.binding.Primary;
-
-import de.codecentric.boot.admin.notify.Notifier;
-import de.codecentric.boot.admin.notify.RemindingNotifier;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
- * 
- * @Title: 为监控的服务添加邮件通知
- * @Package com.lovnx 
- * @author yezhiyuan   
- * @date 2017年6月14日 上午10:18:13 
+ * @author yezhiyuan
  * @version V1.0
+ * @Title: 为监控的服务添加邮件通知
+ * @Package com.lovnx
+ * @date 2017年6月14日 上午10:18:13
  */
 
 @Configuration
 @EnableScheduling
 public class NotifierConfiguration {
-	
+
     @Autowired
     private Notifier notifier;
 
     //服务上线或者下线都通知
-    private String[] reminderStatuses = { "DOWN" };
-    
+    private String[] reminderStatuses = {"DOWN"};
+
     @Bean
     @Primary
     public RemindingNotifier remindingNotifier() {
@@ -40,8 +38,8 @@ public class NotifierConfiguration {
         return remindingNotifier;
     }
 
-//    @Scheduled(fixedRate = 60_000L) 
-//    public void remind() {
-//        remindingNotifier().sendReminders();
-//    }
+    @Scheduled(fixedRate = 60_000L)
+    public void remind() {
+        remindingNotifier().sendReminders();
+    }
 }
--- zuul/src/main/java/com/lovnx/ZuulApplication.java
+++ zuul/src/main/java/com/lovnx/ZuulApplication.java
@@ -1,9 +1,11 @@
 package com.lovnx;
 
 
+import com.lovnx.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -13,9 +15,9 @@ public class ZuulApplication {
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
 	}
 
-//	@Bean
-//	public AccessFilter accessFilter() {
-//		return new AccessFilter();
-//	}
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
 
 }
